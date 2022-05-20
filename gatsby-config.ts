fab282259d9d253e1e8895ed1efcf9fcc9495f9f
Szymon Polny <szymonpolny@outlook.com>
2022-05-20 18:58:44 +0200

    Initial commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,18 +1,23 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `szpolny`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: 'szpolny',
+    siteUrl: 'https://www.yourdomain.tld',
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-react-helmet", "gatsby-transformer-remark", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+  plugins: [
+    'gatsby-plugin-sass',
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: './src/pages/',
+      },
+      __key: 'pages',
     },
-    __key: "pages"
-  }]
+  ],
 };
 
 export default config;
---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+function Layout({ children }: LayoutProps) {
+  return <div>{children}</div>;
+}
+
+export default Layout;
